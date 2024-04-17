cd0394fde2733b81144799f1d363910bd43183df
Klaxon9718 <you9718@naver.com>
2024-04-17 10:11:45 +0900

    event 사용
    https://jaegeun.tistory.com/58

--- react_practice/tic/src/App.js
+++ react_practice/tic/src/App.js
@@ -1,67 +1,72 @@
 import { Component } from "react";
 
 class Content extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      example_state: "이건 Content의 state텍스트 입니다."
-    };
-  }
-
-  render() {
-    return (
-      <div> 
-        {this.state.example_state}
-        <input type="button" value="test 버튼" 
-                onClick={() => {this.setState({example_state:"변경된 state 텍스트입니다."}) }}/>
-      </div>
-    );
-  }
+	render(){
+		return (
+				<div>{this.props.example_props}</div>
+		);
+	}
 }
 
-
-class Title extends Component {
-  render() {
-    return (
-      <div> {this.props.text123} </div>
-    );
-  }
+class Title extends Component{
+	render() {
+		return (
+			<div>
+				이건 Title
+				<input type="button" value="event 버튼"
+						onClick={() => {
+							this.props.exampleEvent("Title에서 전달되는 Text");
+						}}/>
+			</div>
+		)
+	}
 }
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Title text123="App에서 Title로 전달하는 text입니다."/>
-        <Content example_props="App에서 Content로 전달하는 Text입니다."/>
-      </div>
-    );
-  }
+
+class App extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			example_state: "App에서 전달하는 Text"
+		}
+	}
+	
+	setAppText(text){
+		this.setState({
+			example_state: text
+		});
+	}
+
+	render(){
+		return(
+			<div>
+				<Title exampleEvent={(text) => {this.setAppText(text)}}/>
+				<Content example_props={this.state.example_state}/>
+			</div>
+		);
+	}
 }
 
 export default App;
 
 /*
-[props 사용]
-상위 컴포넌트 App에서 
-<tag    전달할 props 이름   전달할 데이터/>
-<Title text123="App에서 Title로 전달하는 text입니다."/>
+[event 사용]
+데이터 전달
+상위 -> 하위 : props와 state이용
+하위 -> 상위 : event 이용
 
-하위 컴포넌트 Title에서
-      {this.props.전달받은 props 이름}
-<div> {this.props.text123} </div>
-으로 prop을 사용
--div 태그 안에서의 자바스크립트 코드는 {}로 감싼다
+[실행 순서]
+1.App 생성자에서 emample_state: "App에서 전달하는 Text"가짐
+2.App 내부의 render실행 -> Title과 Content출력 : Title의 "이건 Title" , button 과 Content의 {this.props.example_props} 문장 (""App에서 전달하는 Text"")이 실행 
+3.onClick이벤트 발생, Title내부의 exampleEvent 실행
+4.App 내부의 <Title exampleEvent={(text) ... 이 실행 > setAppText함수 실행, 인자로 text가 전달
+ [데이터 수신]
+	상위 Component에서는 하위 Component의 event가 발생했을 때 어떤 동작을 할지 정의해주면 된다.
+	App Component에서 exampleEvent가 발생 시 setAppText라는 함수를 호출하게 했으며, 같이 전달되는 text를 받아서 같이 넘겨줬다.
+5.example_state 상태 변경
+6.App 내부의 Content가 실행된다.
 
-[state 사용]
-1. 선언 : 
-  Component의 생성자 constructor에서 선언 및 초기화를 해줬다. 추가하고 싶은 state 변수가 더 있다면 example_state변수  , 뒤에 더 추가하면 된다.
-2. 사용 :
-  사용방법은 props와 마찬가지로 태그 안에서 사용하려면 자바스크립 코드는 { } 로 감싸서 사용하면 된다.
-3. 수정 :
-  state는 수정하려면 this.state.example_state='값' 이런 식으로 하면 안 되고,
-  꼭 setState 함수를 사용해야 한다.
-  setState 함수를 사용하지 않으면 렌더링이 되지 않는다.
-  
+
+	
 [참고 및 출처]
 https://jaegeun.tistory.com/58
 */
