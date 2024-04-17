139cf2bec7a30791f9fdd2c9e3d984922c645f6b
Klaxon9718 <you9718@naver.com>
2024-04-17 09:21:40 +0900

    props와 state 사용
    https://jaegeun.tistory.com/58

--- react_practice/tic/src/App.js
+++ react_practice/tic/src/App.js
@@ -1,10 +1,38 @@
 import { Component } from "react";
 
+class Content extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      example_state: "이건 Content의 state텍스트 입니다."
+    };
+  }
+
+  render() {
+    return (
+      <div> 
+        {this.state.example_state}
+        <input type="button" value="test 버튼" 
+                onClick={() => {this.setState({example_state:"변경된 state 텍스트입니다."}) }}/>
+      </div>
+    );
+  }
+}
+
+
+class Title extends Component {
+  render() {
+    return (
+      <div> {this.props.text123} </div>
+    );
+  }
+}
 class App extends Component {
   render() {
     return (
       <div>
-        Hello World!
+        <Title text123="App에서 Title로 전달하는 text입니다."/>
+        <Content example_props="App에서 Content로 전달하는 Text입니다."/>
       </div>
     );
   }
@@ -13,12 +41,27 @@ class App extends Component {
 export default App;
 
 /*
-App.js
--App이라는 Component가 구현되어 있다. App은 class이며 Component를 상속받는다.
--App Component를 사용하면 <div>Hello World!</div> HTML 태그를 렌더링 하게 된다.
+[props 사용]
+상위 컴포넌트 App에서 
+<tag    전달할 props 이름   전달할 데이터/>
+<Title text123="App에서 Title로 전달하는 text입니다."/>
 
-[Component란]
-Componet는 너무 길고 복잡한 HTML 코드를 모듈화 할 수 있도록 만들어주는 최소 단위를 말한다.
-우리는 Hello World!를 띄우는 간단한 예제를 통해 3줄짜리 <div> 태그를 <App />이라는 하나의 Component로 바꾸는 간단한 일을 했다.
+하위 컴포넌트 Title에서
+      {this.props.전달받은 props 이름}
+<div> {this.props.text123} </div>
+으로 prop을 사용
+-div 태그 안에서의 자바스크립트 코드는 {}로 감싼다
 
+[state 사용]
+1. 선언 : 
+  Component의 생성자 constructor에서 선언 및 초기화를 해줬다. 추가하고 싶은 state 변수가 더 있다면 example_state변수  , 뒤에 더 추가하면 된다.
+2. 사용 :
+  사용방법은 props와 마찬가지로 태그 안에서 사용하려면 자바스크립 코드는 { } 로 감싸서 사용하면 된다.
+3. 수정 :
+  state는 수정하려면 this.state.example_state='값' 이런 식으로 하면 안 되고,
+  꼭 setState 함수를 사용해야 한다.
+  setState 함수를 사용하지 않으면 렌더링이 되지 않는다.
+  
+[참고 및 출처]
+https://jaegeun.tistory.com/58
 */
