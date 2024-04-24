3496117fc10796086d1155c8e5e8a0d960338638
Klaxon9718 <you9718@naver.com>
2024-04-24 13:15:58 +0900

    1.Login.jsx 컴포넌트 분리 2. temp.jsx파일 생성

--- manufact_mate/src/pages/Login.jsx
+++ manufact_mate/src/pages/Login.jsx
@@ -1,12 +1,35 @@
-import * as LoginCom from 'src/pages/components/LoginComponents'
+import * as React from 'react';
+import { Avatar, Button, CssBaseline, TextField, FormControlLabel, Checkbox, Link, Grid, Box, Typography, Container, createTheme, ThemeProvider } from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import * as LoginComponents from 'src/pages/components/LoginComponents'
 
-function Login() {
-    return (
-      <div>
-        <LoginCom.default />
-        창대한 회원가입
-      </div>
-    );
-  }
-  
-  export default Login;
+// TODO remove, this demo shouldn't need to reset the theme.
+
+const defaultTheme = createTheme();
+
+function SignIn() {
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+const defaultTheme = createTheme();
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <LoginComponents.LoginSection getHandleSubmit={handleSubmit} />
+        <LoginComponents.Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default SignIn;
--- manufact_mate/temp.jsx
+++ manufact_mate/temp.jsx
@@ -1,25 +1,41 @@
 import * as React from 'react';
 import {Avatar, Button, CssBaseline, TextField, FormControlLabel, Checkbox, Link, Grid, Box, Typography, Container, createTheme, ThemeProvider} from '@mui/material';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import { CenterFocusStrong } from '@mui/icons-material';
 
-export function Copyright(props) {
-	return (
-	  <Typography variant="body2" color="text.secondary" align="center" {...props}>
-		{'Copyright © '}
-		<Link color="inherit" href="https://mui.com/">
-		  Your Website
-		</Link>{' '}
-		{new Date().getFullYear()}
-		{'.'}
-	  </Typography>
-	);
-  }
 
-export const defaultTheme = createTheme();
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
-export const LoginSection = ({getHandleSubmit}) => (
-  <Box
+// TODO remove, this demo shouldn't need to reset the theme.
+
+const defaultTheme = createTheme();
+
+export default function SignIn() {
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
           sx={{
             marginTop: 8,
             display: 'flex',
@@ -33,7 +49,7 @@ export const LoginSection = ({getHandleSubmit}) => (
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <Box component="form" onSubmit={getHandleSubmit} noValidate sx={{ mt: 1 }}>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
             <TextField
               margin="normal"
               required
@@ -66,13 +82,22 @@ export const LoginSection = ({getHandleSubmit}) => (
             >
               Sign In
             </Button>
-
-            <Grid container justifyContent="center" >
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
               <Grid item>
-                <Link href="#" variant="body2" >Forgot password?</Link>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
               </Grid>
             </Grid>
-
           </Box>
         </Box>
-);
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
