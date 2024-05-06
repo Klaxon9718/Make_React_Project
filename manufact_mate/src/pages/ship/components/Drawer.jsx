8297a0ae2bdffb993089eff6b16be217927249ac
lee <you9718@naver.com>
2024-05-06 21:27:37 +0900

    05.06 백업

--- manufact_mate/src/pages/ship/components/Drawer.jsx
+++ manufact_mate/src/pages/ship/components/Drawer.jsx
@@ -2,22 +2,33 @@ import * as React from 'react';
 import Drawer from '@mui/material/Drawer';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
+import TextField from '@mui/material/TextField';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { FilledInput } from '@mui/material';
 
+const defaultTheme = createTheme(); // 테마 적용
+
+//Drawer내부
+const DrawerList = (
+	<Box sx={{ display: 'flex', width: '100%', height: { xs: 100, sm: 150, md: 250, lg: 300 } }} role="presentation">
+		<Paper sx={{ display: 'flex', width: '100%'}} square={false} elevation={3}>
+			<TextField id="ship_no" label="수주번호" variant="outlined" size="small" InputProps={{
+    readOnly: true,
+  }}/>
+
+		</Paper>
+	</Box>
+);
 
 export default function BottomDrawer(props) {
 
-	//Drawer내부
-	const DrawerList = (
-		<Box sx={{  display: 'flex', width: 'flex', height : {xs:100, sm:150, md:250, lg:300} }} role="presentation">
-			<Paper square={false} elevation={3}>
-				
-			</Paper>
-		</Box>
-	);
 
-	return(
-		<Drawer anchor={'bottom'} open={props.isopen} onClose={props.onClose}>
-		{DrawerList}
-	  	</Drawer>
+
+	return (
+		<ThemeProvider theme={defaultTheme}>
+			<Drawer anchor={'bottom'} open={props.isopen} onClose={props.onClose}>
+				{DrawerList}
+			</Drawer>
+		</ThemeProvider>
 	);
 }
